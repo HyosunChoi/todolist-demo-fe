57660d89fb98cfa1dd5705d48ac12ce9c67a9331
HyosonChoi <chsnizzi0@gmail.com>
2024-10-18 23:31:41 +0900

    TodoItem.js : added isCompled status

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -3,10 +3,29 @@ import { Col, Row } from "react-bootstrap";
 
 const TodoItem = ({item, deleteTask}) => {
 
-  const [isCompleted, setIsCompleted] = useState(false);
+  const [isCompleted, setIsCompleted] = useState(item.isCompleted);
 
-  const handleComplete = () => {
-    setIsCompleted(!isCompleted);
+  const handleComplete = async() => {
+    try {
+      //상태변경
+      const updateStatus = await fetch(`/tasks/${item.id}`, {
+        method: "PUT",
+        headers : {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({isCompleted: updateStatus}),
+      })
+
+      if(!Response.ok) {
+        throw new Error("Task update failed");
+      }
+
+      console.log("Updated successfully");      
+    } catch (err) {
+      console.error("error", err);
+      //에러발생 시 원래 상태로 복구
+      setIsCompleted(!isCompleted);
+    }
   };
 
   return (
